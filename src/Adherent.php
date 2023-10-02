c4eccbb95ba9e5531ff579300d5fdb978e13f9c3
Arthur LY <gtab4A@gmail.com>
2023-10-02 16:57:30 +0200

    Classe Adherent

---
+++ src/Adherent.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App;
+
+
+class Adherent
+{
+    private string $numero;
+    private string $nom;
+    private string $prenom;
+    private string $email;
+    private \DateTime $dateAdhesion;
+
+    /**
+     * @param string $nom
+     * @param string $prenom
+     * @param string $email
+     * @param ?string $dateAdhesion
+     */
+    public function __construct(string $nom, string $prenom, string $email, ?string $dateAdhesion = null) {
+        $this->numero = $this->genererNumero();
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        if (gettype($dateAdhesion) == "string") {
+            $dateAdhesion = \DateTime::createFromFormat("d/m/Y", $dateAdhesion);
+        } else {
+            $dateAdhesion = new \DateTime();
+        }
+        $this->dateAdhesion = $dateAdhesion;
+    }
+
+    private function genererNumero(): string {
+        return "AD-" . str_pad(strval(rand(0, 999999)), 6, "0", STR_PAD_LEFT);
+    }
+
+    public function getInformations(): string {
+        $numero = "Numero : $this->numero";
+        $nom = "Nom : $this->nom";
+        $prenom = "Prenom : $this->prenom";
+        $email = "Email : $this->email";
+        $dateAdhesion = "Date d'adhesion : {$this->dateAdhesion->format("d/m/Y")}";
+        return "$numero\n$nom\n$prenom\n$email\n$dateAdhesion";
+    }
+
+    public function checkAdhesionValide(): bool {
+        if ($this->dateAdhesion->diff(new \DateTime())->y != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public function renouvelerAdhesion(): bool {
+        try {
+            $this->dateAdhesion->add(new \DateInterval("P1Y"));
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumero(): string {
+        return $this->numero;
+    }
+
+    /**
+     * @param string $numero
+     */
+    public function setNumero(string $numero): void {
+        $this->numero = $numero;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom(string $nom): void {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom(string $prenom): void {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateAdhesion(): \DateTime {
+        return $this->dateAdhesion;
+    }
+
+    /**
+     * @param \DateTime $dateAdhesion
+     */
+    public function setDateAdhesion(\DateTime $dateAdhesion): void {
+        $this->dateAdhesion = $dateAdhesion;
+    }
+
+}
--- tests/unitaires/adherent.php
+++ tests/unitaires/adherent.php
@@ -1,1 +1,57 @@
 <?php
+require_once './vendor/autoload.php';
+require_once "./tests/utils/couleurs.php";
+echo PHP_EOL;
+echo GREEN_BACKGROUND . BLACK;
+echo "Tests : classe Adherent";
+echo RESET;
+echo PHP_EOL;
+
+echo "Test n°1 : vérifier que la date d’adhésion, si non précisée à la création, est égale à la date du jour \n";
+$adherent = new \App\Adherent("Price", "Chloe", "chloeprice@gmail.com");
+$dateNow = new DateTime();
+if ($adherent->getDateAdhesion()->format("d/m/Y") == $dateNow->format("d/m/Y")) {
+    echo GREEN . "Test ok" . RESET;
+} else {
+    echo RED . "Test pas ok" . RESET;
+}
+
+echo "\n";
+echo "Test n°2 : vérifier que la date d’adhésion, si précisée à la création, est bien prise en compte \n";
+$dateAdhesion = DateTime::createFromFormat("d/m/Y", "12/12/2000");
+$adherent = new \App\Adherent("Price", "Chloe", "chloeprice@gmail.com", "12/12/2000");
+if ($adherent->getDateAdhesion() == $dateAdhesion) {
+    echo GREEN . "Test ok" . RESET;
+} else {
+    echo RED . "Test pas ok" . RESET;
+}
+echo "\n";
+echo "Test n°3 : vérifier que le numéro d’adhérent, à la création, est valide\n";
+$adherent = new \App\Adherent("Price", "Chloe", "chloeprice@gmail.com", "12/12/2000");
+if (str_contains($adherent->getNumero(), "AD-")
+    && strlen($adherent->getNumero()) == 9
+    && is_numeric(substr($adherent->getNumero(), -6))) {
+    echo GREEN . "Test ok" . RESET;
+} else {
+    echo RED . "Test pas ok" . RESET;
+}
+
+echo "\n";
+echo "Test n°4 : vérifier que l’adhésion est valable (valide) quand la date d’adhésion n’est pas dépassée (moins d’un an) \n";
+$adherent = new \App\Adherent("Price", "Chloe", "chloeprice@gmail.com", "12/12/2022");
+if ($adherent->checkAdhesionValide()){
+    echo GREEN . "Date d'adhesion : {$adherent->getDateAdhesion()->format("d/m/Y")}, date valide" . RESET;
+} else {
+    echo RED . "Date d'adhesion : {$adherent->getDateAdhesion()->format("d/m/Y")}, date invalide" . RESET;
+}
+
+echo "\n";
+echo "Test n°5 : vérifier que l’adhésion est renouvelée \n";
+$adherent = new \App\Adherent("Price", "Chloe", "chloeprice@gmail.com", "12/12/2022");
+echo "Date initiale de l'adherent : ".$adherent->getDateAdhesion()->format("d/m/Y")."\n";
+if ($adherent->renouvelerAdhesion()){
+    echo "Date renouvelée de l'adherent : ".$adherent->getDateAdhesion()->format("d/m/Y")."\n";
+    echo GREEN . "Test ok" . RESET;
+} else {
+    echo RED . "Test pas ok" . RESET;
+}
